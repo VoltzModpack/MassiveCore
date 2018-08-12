6ffde37b6bbaf185d4581ebf1a3b694b575bc710
kmecpp <kmecpp@gmail.com>
2018-08-12 17:26:08 -0400

    Disable BoardUtil and overwrite version to be the same as Factions

--- src/com/massivecraft/massivecore/MassiveCore.java
+++ src/com/massivecraft/massivecore/MassiveCore.java
@@ -82,55 +82,58 @@ import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
 
-public class MassiveCore extends MassivePlugin
-{
+public class MassiveCore extends MassivePlugin {
 	// -------------------------------------------- //
 	// COMMON CONSTANTS
 	// -------------------------------------------- //
-	
+
 	public final static String INSTANCE = "instance";
 	public final static String DEFAULT = "default";
 	public static final String NONE = Txt.parse("<silver>none");
-	
+
 	public final static Set<String> NOTHING = MUtil.treeset("", "none", "null", "nothing");
 	public final static Set<String> REMOVE = MUtil.treeset("clear", "c", "delete", "del", "d", "erase", "e", "remove", "rem", "r", "reset", "res");
 	public final static Set<String> NOTHING_REMOVE = MUtil.treeset("", "none", "null", "nothing", "clear", "c", "delete", "del", "d", "erase", "e", "remove", "rem", "r", "reset", "res");
-	
-	
+
 	// -------------------------------------------- //
 	// INSTANCE & CONSTRUCT
 	// -------------------------------------------- //
-	
+
 	private static MassiveCore i;
-	public static MassiveCore get() { return i; }
-	public MassiveCore() { i = this; }
-	
+
+	public static MassiveCore get() {
+		return i;
+	}
+
+	public MassiveCore() {
+		i = this;
+	}
+
 	// -------------------------------------------- //
 	// STATIC
 	// -------------------------------------------- //
-	
+
 	public static Random random = new Random();
 	public static Gson gson = getMassiveCoreGsonBuilder().create();
-	
-	public static GsonBuilder getMassiveCoreGsonBuilder()
-	{
+
+	public static GsonBuilder getMassiveCoreGsonBuilder() {
 		// Create
 		GsonBuilder ret = new GsonBuilder();
-		
+
 		// Basic Behavior
 		ret.setPrettyPrinting();
 		ret.disableHtmlEscaping();
 		ret.excludeFieldsWithModifiers(Modifier.TRANSIENT);
-		
+
 		// Raw Adapters
 		ret.registerTypeAdapter(JsonNull.class, AdapterJsonElement.get());
 		ret.registerTypeAdapter(JsonPrimitive.class, AdapterJsonElement.get());
 		ret.registerTypeAdapter(JsonArray.class, AdapterJsonElement.get());
 		ret.registerTypeAdapter(JsonObject.class, AdapterJsonElement.get());
-		
+
 		// Enumeration Annotation Dodge
 		ret.registerTypeAdapterFactory(AdapterModdedEnumType.ENUM_FACTORY);
-		
+
 		// Massive Containers
 		ret.registerTypeAdapter(MassiveList.class, AdapterMassiveList.get());
 		ret.registerTypeAdapter(MassiveListDef.class, AdapterMassiveList.get());
@@ -142,10 +145,10 @@ public class MassiveCore extends MassivePlugin
 		ret.registerTypeAdapter(MassiveTreeMapDef.class, AdapterMassiveTreeMap.get());
 		ret.registerTypeAdapter(MassiveTreeSet.class, AdapterMassiveTreeSet.get());
 		ret.registerTypeAdapter(MassiveTreeSetDef.class, AdapterMassiveTreeSet.get());
-		
+
 		// Entries (Is this still needed?)
 		ret.registerTypeAdapter(Entry.class, AdapterEntry.get());
-		
+
 		// Assorted Custom
 		ret.registerTypeAdapter(BackstringSet.class, AdapterBackstringSet.get());
 		ret.registerTypeAdapter(PS.class, PSAdapter.get());
@@ -155,134 +158,134 @@ public class MassiveCore extends MassivePlugin
 		// Mson
 		ret.registerTypeAdapter(Mson.class, AdapterMson.get());
 		ret.registerTypeAdapter(MsonEvent.class, AdapterMsonEvent.get());
-		
+
 		// Banner Patterns Upgrade Adapter
 		// NOTE: Must come after the "MassiveContainers" section for priority.
-		Type typeBannerPatterns = new TypeToken<MassiveListDef<DataBannerPattern>>(){}.getType();
+		Type typeBannerPatterns = new TypeToken<MassiveListDef<DataBannerPattern>>() {
+		}.getType();
 		ret.registerTypeAdapter(typeBannerPatterns, AdapterBannerPatterns.get());
-		
+
 		// ItemStack
 		ret.registerTypeAdapter(ItemStack.class, AdapterItemStack.get());
 		Class<?> classCraftItemStack = NmsItemStackCreate17R4P.getClassCraftItemStackCatch();
 		if (classCraftItemStack != null) ret.registerTypeAdapter(classCraftItemStack, AdapterItemStack.get());
-		
+
 		// Inventory
 		ret.registerTypeAdapter(Inventory.class, AdapterInventory.get());
 		ret.registerTypeAdapter(PlayerInventory.class, AdapterPlayerInventory.get());
-		
+
 		// Storage
 		ret.registerTypeAdapter(EntityInternalMap.class, AdapterEntityInternalMap.get());
-		
+
 		// Return
 		return ret;
 	}
-	
-	public static String getServerId() { return ConfServer.serverid; }
-	public static String getTaskServerId() { return MassiveCoreMConf.get().taskServerId; }
-	public static boolean isTaskServer()
-	{
+
+	public static String getServerId() {
+		return ConfServer.serverid;
+	}
+
+	public static String getTaskServerId() {
+		return MassiveCoreMConf.get().taskServerId;
+	}
+
+	public static boolean isTaskServer() {
 		String taskServerId = getTaskServerId();
 		if (taskServerId == null) return true;
 		if (getServerId().equals(taskServerId)) return true;
 		return false;
 	}
-	
+
 	// -------------------------------------------- //
 	// LOAD
 	// -------------------------------------------- //
-	
+
 	@Override
-	public void onLoadInner()
-	{
+	public void onLoadInner() {
 		// These util classes are not automatically loaded/resolved when MassiveCore is being loaded.
 		// However they need to be loaded to ensure async safety.
 		// This fixes a race condition within the asynchronous class loader (LinkageError).
 		ReflectionUtil.forceLoadClasses(
-			ContainerUtil.class,
-			EventUtil.class,
-			IntervalUtil.class,
-			InventoryUtil.class,
-			PeriodUtil.class,
-			RecipeUtil.class,
-			SignUtil.class,
-			SmokeUtil.class,
-			TimeUnit.class,
-			TimeDiffUtil.class
-		);
+				ContainerUtil.class,
+				EventUtil.class,
+				IntervalUtil.class,
+				InventoryUtil.class,
+				PeriodUtil.class,
+				RecipeUtil.class,
+				SignUtil.class,
+				SmokeUtil.class,
+				TimeUnit.class,
+				TimeDiffUtil.class);
 	}
-	
+
 	// -------------------------------------------- //
 	// ENABLE
 	// -------------------------------------------- //
-	
+
 	@Override
-	public void onEnableInner()
-	{
+	public void onEnableInner() {
 		// This is safe since all plugins using Persist should bukkit-depend this plugin.
 		// Note this one must be before preEnable. dooh.
 		// TODO: Create something like "deinit all" (perhaps a forloop) to readd this.
 		// TODO: Test and ensure reload compat.
 		// Coll.instances.clear();
-		
+
 		// Load Server Config
 		ConfServer.get().load();
-		
+
 		// Setup IdUtil
 		IdUtil.setup();
-		
+
 		// Setup RegistryType
 		RegistryType.registerAll();
-		
+
 		MigratorUtil.addJsonRepresentation(ItemStack.class, DataItemStack.class);
 		MigratorUtil.addJsonRepresentation(Inventory.class, null);
 		MigratorUtil.setTargetVersion(Inventory.class, MigratorUtil.getTargetVersion(DataItemStack.class));
-		
+
 		// Activate
 		this.activateAuto();
 
 		// These must be activated after nms
 		this.activate(
 
-			// Writer,
-			WriterItemStack.class,
-			
-			// Util
-			PlayerUtil.class,
-			BoardUtil.class
+				// Writer,
+				WriterItemStack.class,
+
+				// Util
+				PlayerUtil.class
+		//			BoardUtil.class
 		);
 
 		// Start the examine threads
 		// Start AFTER initializing the MConf, because they rely on the MConf.
 		if (ConfServer.localPollingEnabled) ModificationPollerLocal.get().start();
 		ModificationPollerRemote.get().start();
-		
+
 		// Delete Files (at once and additionally after all plugins loaded)
 		MassiveCoreTaskDeleteFiles.get().run();
 		Bukkit.getScheduler().scheduleSyncDelayedTask(this, MassiveCoreTaskDeleteFiles.get());
 	}
 
 	@Override
-	public List<Class<?>> getClassesActiveColls()
-	{
+	public List<Class<?>> getClassesActiveColls() {
 		List<Class<?>> ret = this.getClassesActive(null, Coll.class);
 
 		return ret;
 	}
-	
+
 	@Override
-	public List<Class<?>> getClassesActiveNms()
-	{
+	public List<Class<?>> getClassesActiveNms() {
 		List<Class<?>> ret = super.getClassesActiveNms();
-		
+
 		ret.remove(NmsBasics.class);
 		ret.add(0, NmsBasics.class);
-		
+
 		return ret;
 	}
-	
+
 	@Override
-	public List<Class<?>> getClassesActiveMixins()
-	{
+	public List<Class<?>> getClassesActiveMixins() {
 		List<Class<?>> ret = super.getClassesActiveMixins();
 
 		ret.remove(MixinEvent.class);
@@ -290,18 +293,17 @@ public class MassiveCore extends MassivePlugin
 
 		return ret;
 	}
-	
+
 	// -------------------------------------------- //
 	// DISABLE
 	// -------------------------------------------- //
-	
+
 	@Override
-	public void onDisable()
-	{
+	public void onDisable() {
 		super.onDisable();
 		ModificationPollerLocal.get().interrupt();
 		ModificationPollerRemote.get().interrupt();
-		
+
 		MassiveCoreTaskDeleteFiles.get().run();
 		IdUtil.saveCachefileDatas();
 	}
